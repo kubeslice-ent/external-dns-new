f3f9f95cabc94f7d1f6b7830ebd1f1efc002f7ed
William Marchesi <will@marchesi.io>
2022-09-06 13:39:13 -0400

    Expose nginx instances to the public Update k6 Script
    Signed-off-by: William Marchesi <will@marchesi.io>

--- demo/script.js
+++ demo/script.js
@@ -1,8 +1,19 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export const options = {
+    vus: 5,
+    duration: '1h',
+    dns: {
+        ttl: '0',
+        //select: 'first',
+        policy: 'any',
+      },
+      noConnectionReuse: true,
+};
+
 export default function () {
-  http.get('https://test.k6.io');
-  sleep(1);
+    http.get('http://ns1.wmar.io:30081');
+    //sleep(0.1);
 }
 
